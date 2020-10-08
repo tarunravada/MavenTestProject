650561fc0e852fb5b5347a7a6b77c029c52e5984
tarunravada <tarunravada@gmail.com>
2020-10-08 15:27:53 -0400

    first commit

---
+++ MavenTest/src/test/java/helloWorld/TestHelloWorld.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testHelloWorld() {
+		System.out.println("---Hello World---");
+	}
+}
